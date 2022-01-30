25b8a4cc252ca7b0d9d7be6abb43e7495c477ae4
Reymom <ramon_gs95@hotmail.com>
2022-01-30 21:31:29 +0100

    refractor wallet, plug connection and fixes in templates

--- application/go/internal/routes/routes.go
+++ application/go/internal/routes/routes.go
@@ -4,18 +4,23 @@ import (
 	"html/template"
 	"net/http"
 	"os"
-	"path/filepath"
 
-	fabricConfig "github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
 	"github.com/reymom/bsm-hyperledger/application/go/cmd/app/config"
+	"github.com/reymom/bsm-hyperledger/application/go/internal/connection"
 	"github.com/rs/zerolog/log"
 )
 
-var templates *template.Template
-var auctionContract *gateway.Contract
-var logisticsContract *gateway.Contract
-var loggedIn bool
+var (
+	templates        *template.Template
+	connectionConfig *config.Config
+
+	gw          *gateway.Gateway
+	gwContracts []connection.GatewayContract
+
+	authUser = new(connection.Login)
+	loggedIn bool
+)
 
 func GenerateRoutes(conf *config.Config) (http.Handler, error) {
 	e := os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
@@ -23,44 +28,7 @@ func GenerateRoutes(conf *config.Config) (http.Handler, error) {
 		log.Err(e).Msg("Error setting env variable DISCOVERY_AS_LOCALHOST")
 	}
 
-	wallet, e := gateway.NewFileSystemWallet("wallet")
-	if e != nil {
-		log.Err(e).Msg("Failed to create wallet")
-	}
-
-	if !wallet.Exists("appUser") {
-		e = populateWallet(wallet)
-		if e != nil {
-			log.Err(e).Msg("Failed to populate wallet contents")
-		}
-	}
-
-	ccpPath := filepath.Join(
-		"..",
-		"..",
-		"organizations",
-		"peerOrganizations",
-		"supplier1.steelplatform.com",
-		"connection-supplier1.yaml",
-	)
-
-	gw, e := gateway.Connect(
-		gateway.WithConfig(fabricConfig.FromFile(filepath.Clean(ccpPath))),
-		gateway.WithIdentity(wallet, "appUser"),
-	)
-	if e != nil {
-		log.Err(e).Msg("Failed to connect to gateway")
-	}
-	defer gw.Close()
-
-	network, e := gw.GetNetwork("public1channel")
-	if e != nil {
-		log.Err(e).Msg("Failed to get network")
-	}
-
-	auctionContract = network.GetContract("auction")
-	log.Info().Msgf("Loaded Contract:%s", auctionContract.Name())
-	// logisticsContract = network.GetContract("logistics")
+	connectionConfig = conf
 
 	//template routing
 	funcMap := template.FuncMap{
@@ -77,11 +45,13 @@ func GenerateRoutes(conf *config.Config) (http.Handler, error) {
 	mux := http.NewServeMux()
 
 	mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./www/static/css"))))
-
+	mux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./www/static/images"))))
 	mux.Handle("/home", http.HandlerFunc(homeHandler))
-	mux.Handle("/login", http.HandlerFunc(loginHandler))
-	mux.Handle("/logout", http.HandlerFunc(logoutHandler))
 
+	e = generateLoginRoutes(mux)
+	if e != nil {
+		return nil, e
+	}
 	e = generateAuctionRoutes(mux)
 	if e != nil {
 		return nil, e
